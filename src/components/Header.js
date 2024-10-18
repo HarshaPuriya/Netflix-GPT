3536974551acaa4dbe6fcf8a9cbc1c2ff22f22b9
Harsha Puriya <harshapuriya@gmail.com>
2024-10-18 12:17:20 +0530

    Not logged in juser redirect to sign up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { signOut } from "firebase/auth";
 import { auth } from '../utils/firebase';
 import { useNavigate } from 'react-router-dom';
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from 'react-redux'
+import { addUser, removeUser } from '../utils/userSlice'
 
 const Header = () => {
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleSignOut = () => {
     signOut(auth).then(() => {
-      navigate("/")
     }).catch((error) => {
       // An error happened.
     });
   }
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+          const {uid, email, displayName, photoURL} = user;
+          dispatch(addUser(
+            {uid: uid, 
+              email : email, 
+              displayName : displayName, 
+              photoURL : photoURL}
+            ));  
+            navigate("/browse");
+        } 
+        else {
+          dispatch(removeUser());
+          navigate("/");
+        }
+      });
+
+}, []);
 
   return (
     <div className='absolute bg-gradient-to-b from-black z-10 w-full flex justify-between'>
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,6 @@ import Header from './Header'
 import { checkValiddata } from '../utils/validate';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import {auth} from "../utils/firebase";
-import { useNavigate } from 'react-router-dom';
 import {updateProfile } from "firebase/auth";
 
 
@@ -12,7 +11,6 @@ const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
 
-  const navigate = useNavigate();
 
   const name = useRef(null);
   const email = useRef(null);
@@ -34,7 +32,7 @@ const Login = () => {
           updateProfile(user, {
             displayName: name.current.value, photoURL: "https://example.com/jane-q-user/profile.jpg"
           }).then(() => {
-            navigate("/browse");
+            
           }).catch((error) => {
             setErrorMessage(error.message);
           });
@@ -53,8 +51,6 @@ const Login = () => {
         .then((userCredential) => {
        // Signed in 
           const user = userCredential.user;
-          console.log(user);
-          navigate("/browse")
         })
         .catch((error) => {
           const errorCode = error.code;
